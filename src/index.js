d946767ce3840035c1c911f0ad213ae6a274e356
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2023-10-28 15:23:26 -0500

    Add gravity and get mouse click to change object position, add a pipe object

--- src/index.js
+++ src/index.js
@@ -9,24 +9,66 @@ const config = {
     // Arcade Physics is a light-weight 2D physics engine made for games.
     // It has AABB collision support and a simple separation of velocity and position.
     default: 'arcade',
-    // arcade: {
-    //   gravity: { y: 200 },
-    // },
+    // apply gravity to all game objects into the scene
+    arcade: {
+      debug: true,
+    },
   },
   scene: {
     preload,
     create,
+    update, // will be called on every frame
   },
 };
 
 // Loading assests, such as images, audio, and other data files is done in the preload() function.
 // the this context is the Scene to which the loader belongs.
 function preload() {
-  this.load.image('sky', 'assets/sky.png');
+  this.load.image('sky-bg', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
+let bird = null;
+let pipe = null;
+let flatVelocity = 250;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
+
 function create() {
-  this.add.image(400, 300, 'sky');
+  // Add an image to the scene. and set the origin to the top left corner.
+  this.add.image(0, 0, 'sky-bg').setOrigin(0);
+
+  bird = this.physics.add
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird')
+    .setOrigin(0);
+  bird.body.gravity.y = 400;
+
+  pipe = this.physics.add.sprite(300, 100, 'pipe').setOrigin(0);
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
+}
+
+// 60 frames per second
+// 60 times per second
+
+// if bird y position is small than 0 or greater than height of the canvas
+// then game over alert
+function update(time, delta) {
+  if (bird.y > config.height || bird.y < -bird.height) {
+    // alert('Game Over');
+    restartBirdPosition();
+  }
+}
+
+function flap() {
+  bird.setVelocityY(-flatVelocity);
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
 }
 
 new Phaser.Game(config);
